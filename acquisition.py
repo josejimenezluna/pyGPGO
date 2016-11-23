04ca9b7b6f28140ab7a186c1e7c842f6576a5613
hawk31 <hawk31@gmail,com>
2016-11-23 21:14:46 +0100

    fixed mistake

--- acquisition.py
+++ acquisition.py
@@ -6,19 +6,19 @@ class Acquisition:
 		self.params = params
 		if mode == 'ExpectedImprovement':
 			self.f = self.ExpectedImprovement
-		if mode == 'ProbabilityImprovement':
+		elif mode == 'ProbabilityImprovement':
 			self.f = self.ProbabilityImprovement
-		if mode == 'UCB':
-			self.f = self.YetAnotherWay
+		elif mode == 'UCB':
+			self.f = self.UCB
 		else:
 			raise ValueError('Not recognised acquisition function')
 	def ExpectedImprovement(self, xbest, mean, std):
 		return norm.cdf((mean - xbest) / std)
 	def ProbabilityImprovement(self, xbest, mean, std):
 		z = (mean - xbest) / std
-		return (mean - xbest) * norm.cdf(z) + var * norm.pdf(z)
+		return (mean - xbest) * norm.cdf(z) + std * norm.pdf(z)
 	def UCB(self, xbest, mean, std, beta):
 		return mean + beta * std
-	def eval(self, xbest, mean, var):
-		return self.f(self, xbest, mean, var, **self.params)
+	def eval(self, xbest, mean, std):
+		return self.f(xbest, mean, std, **self.params)
 	
