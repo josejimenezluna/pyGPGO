c5055e1f09f393e2332c4c3e242073b96f693688
hawk31 <hawk31@gmail.com>
2016-11-24 16:05:44 +0100

    Advancing

--- GPGO.py
+++ GPGO.py
@@ -1,6 +1,8 @@
 import numpy as np
 from GPRegressor import GPRegressor
 from scipy.optimize import minimize
+from cma import fmin
+from collections import OrderedDict
 
 class GPGO:
 	def __init__(self, GPRegressor, Acquisition, f, parameter_dict):
@@ -15,7 +17,7 @@ class GPGO:
 		self.parameter_range = [p[1] for p in self.parameter_value]
 
 	def _sampleParam(self):
-		d = {}
+		d = OrderedDict()
 		for index, param in enumerate(self.parameter_key):
 			if parameter_type[index] == 'int':
 				d[param] = np.random.randint(self.parameter_range[index][0], self.parameter_range[index][1])
@@ -44,9 +46,24 @@ class GPGO:
 		return self.A.eval(self.tau, new_mean, new_std)
 		
 	def _optimizeAcq(self, method = 'L-BFGS-B', nstart = 100):
+		start_points = [list(self._sampleParam().values()) for i in range(nstart)]
+		x_best = np.empty((n_start, len(self.parameter_key)))
+		f_best = np.empty((n_start,))
 		#TODO use L-BFGS-B or CMA-ES.
 		if method == 'L-BFGS-B':
 
-		elif method = 'CMA-ES':
-					
+		elif method = 'CMA-ES': # This can be parallelized
+			for index, start_point in enumerate(start_points):
+				res = fmin(-self._acqWrapper, start_point)
+				x_best[index], f_best[index] = res[0], res[1]
+		self.best = x_best[np.argmin(f_best)]		
+	
+	def updateGP(self):
+		self.GP.update(self.best)
+
+	def run(self, max_iter = 10):
+		self._firstRun()
+		for iteration in range(max_iter):
+			self._optimizeAcq()
+			self.updateGP()
 		
