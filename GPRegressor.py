145ccb2720d428a0c4bb9248fe07864d3f0da180
hawk31 <hawk31@gmail.com>
2017-02-14 16:32:38 +0100

    Code refactor.

--- GPRegressor.py
+++ GPRegressor.py
@@ -1,67 +1,69 @@
 import numpy as np
 from scipy.linalg import cholesky, solve
 from collections import OrderedDict
-from scipy.optimize import minimize 
+from scipy.optimize import minimize
+
 
 class GPRegressor:
-	def __init__(self, covfunc, sigma = 0):
-		self.covfunc = covfunc
-		self.sigma = sigma
-	def fit(self, X, y):
-		self.X = X
-		self.y = y
-		self.nsamples = self.X.shape[0]
-		self.K = self.covfunc.K(self.X, self.X)
-		self.L = cholesky(self.K + self.sigma * np.eye(self.nsamples)).T
-		self.alpha = solve(self.L.T, solve(self.L, y))
-		self.logp = -.5 * np.dot(self.y, self.alpha) - np.sum(np.log(np.diag(self.L))) - self.nsamples/2 * np.log(2 * np.pi)
+    def __init__(self, covfunc, sigma=0):
+        self.covfunc = covfunc
+        self.sigma = sigma
 
-	def param_grad(self, X, y, k_param):
-		k_param_key = list(k_param.keys())
-		covfunc = self.covfunc.__class__(**k_param)
-		n = X.shape[0]
-		K = covfunc.K(X, X)
-		L = cholesky(K + self.sigma * np.eye(n)).T
-		alpha = solve(L.T, solve(L, y))
-		# Compute gradient matrix for each parameter
-		grads = []
-		for param in k_param_key:
-			gradK = covfunc.gradK(X, X, param = param)
-			inner = np.dot(np.atleast_2d(alpha).T, np.atleast_2d(alpha)) - np.linalg.inv(K)
-			gradK = .5 * np.trace(np.dot(inner, gradK))
-			grads.append(gradK)
-		return np.array(grads)
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        self.nsamples = self.X.shape[0]
+        self.K = self.covfunc.K(self.X, self.X)
+        self.L = cholesky(self.K + self.sigma * np.eye(self.nsamples)).T
+        self.alpha = solve(self.L.T, solve(self.L, y))
+        self.logp = -.5 * np.dot(self.y, self.alpha) - np.sum(np.log(np.diag(self.L))) - self.nsamples / 2 * np.log(
+            2 * np.pi)
 
-	def _lmlik(self, param_vector, param_key):
-		k_param = OrderedDict()
-		for k, v in zip(param_key, param_vector):
-			k_param[k] = v
-		self.covfunc = self.covfunc.__class__(**k_param)
-		self.fit(self.X, self.y)
-		return(- self.logp)
+    def param_grad(self, X, y, k_param):
+        k_param_key = list(k_param.keys())
+        covfunc = self.covfunc.__class__(**k_param)
+        n = X.shape[0]
+        K = covfunc.K(X, X)
+        L = cholesky(K + self.sigma * np.eye(n)).T
+        alpha = solve(L.T, solve(L, y))
+        # Compute gradient matrix for each parameter
+        grads = []
+        for param in k_param_key:
+            gradK = covfunc.gradK(X, X, param=param)
+            inner = np.dot(np.atleast_2d(alpha).T, np.atleast_2d(alpha)) - np.linalg.inv(K)
+            gradK = .5 * np.trace(np.dot(inner, gradK))
+            grads.append(gradK)
+        return np.array(grads)
 
-	def optHyp(self, param_key, param_bounds):
-		x0 = np.repeat(1, len(param_key))
-		res = minimize(self._lmlik, x0 = x0 ,args = (param_key), method='L-BFGS-B', bounds = param_bounds)
-		opt_param = res.x
-		k_param = OrderedDict()
-		for k, x in zip(param_key, opt_param):
-			k_param[k] = x
-		self.covfunc = self.covfunc.__class__(**k_param)
-		self.fit(self.X, self.y) 	
-			
-	def predict(self, Xstar, return_std = False):
-		Xstar = np.atleast_2d(Xstar)
-		kstar = self.covfunc.K(self.X, Xstar).T
-		fmean = np.dot(kstar, self.alpha)
-		v = solve(self.L, kstar.T)
-		fcov = self.covfunc.K(Xstar, Xstar) - np.dot(v.T, v)
-		if return_std:
-			fcov = np.diag(fcov)
-		return fmean, fcov
+    def _lmlik(self, param_vector, param_key):
+        k_param = OrderedDict()
+        for k, v in zip(param_key, param_vector):
+            k_param[k] = v
+        self.covfunc = self.covfunc.__class__(**k_param)
+        self.fit(self.X, self.y)
+        return (- self.logp)
 
-	def update(self, xnew, ynew):
-		y = np.concatenate((self.y, ynew), axis = 0)
-		X = np.concatenate((self.X, xnew), axis = 0)
-		self.fit(X, y)
+    def optHyp(self, param_key, param_bounds):
+        x0 = np.repeat(1, len(param_key))
+        res = minimize(self._lmlik, x0=x0, args=(param_key), method='L-BFGS-B', bounds=param_bounds)
+        opt_param = res.x
+        k_param = OrderedDict()
+        for k, x in zip(param_key, opt_param):
+            k_param[k] = x
+        self.covfunc = self.covfunc.__class__(**k_param)
+        self.fit(self.X, self.y)
 
+    def predict(self, Xstar, return_std=False):
+        Xstar = np.atleast_2d(Xstar)
+        kstar = self.covfunc.K(self.X, Xstar).T
+        fmean = np.dot(kstar, self.alpha)
+        v = solve(self.L, kstar.T)
+        fcov = self.covfunc.K(Xstar, Xstar) - np.dot(v.T, v)
+        if return_std:
+            fcov = np.diag(fcov)
+        return fmean, fcov
+
+    def update(self, xnew, ynew):
+        y = np.concatenate((self.y, ynew), axis=0)
+        X = np.concatenate((self.X, xnew), axis=0)
+        self.fit(X, y)
--- covfunc.py
+++ covfunc.py
@@ -5,104 +5,115 @@ import numpy as np
 from scipy.special import gamma, kv
 from scipy.spatial.distance import cdist
 
+
 def l2norm(x, xstar):
-	return(np.sqrt(np.sum((x - xstar)**2, axis = 1)))
+    return (np.sqrt(np.sum((x - xstar) ** 2, axis=1)))
+
 
 def l2norm_(X, Xstar):
-	return cdist(X, Xstar)
+    return cdist(X, Xstar)
+
 
 class squaredExponential:
-	def __init__(self, l = 1, sigma2f = 1, sigma2n = 0):
-		self.l = l
-		self.sigma2f = sigma2f
-		self.sigma2n = sigma2n
+    def __init__(self, l=1, sigma2f=1, sigma2n=0):
+        self.l = l
+        self.sigma2f = sigma2f
+        self.sigma2n = sigma2n
 
-	def K(self, X, Xstar):
-		r = l2norm_(X, Xstar)
-		return(np.exp(-.5 * r ** 2 / self.l ** 2))
+    def K(self, X, Xstar):
+        r = l2norm_(X, Xstar)
+        return (np.exp(-.5 * r ** 2 / self.l ** 2))
 
-	def gradK(self, X, Xstar, param = 'l'):
-		if param == 'l':
-			r = l2norm_(X, Xstar)
-			num = r**2 * np.exp(-r**2 /(2* self.l**2))
-			den = self.l ** 3
-			l_grad = num / den
-			return(l_grad)
-		else:
-			raise ValueError('Param not found')
+    def gradK(self, X, Xstar, param='l'):
+        if param == 'l':
+            r = l2norm_(X, Xstar)
+            num = r ** 2 * np.exp(-r ** 2 / (2 * self.l ** 2))
+            den = self.l ** 3
+            l_grad = num / den
+            return (l_grad)
+        else:
+            raise ValueError('Param not found')
+
 
 class matern:
-	def __init__(self, v = 1, l = 1):
-		self.v, self.l = v, l
-	def k(self, x, xstar):
-		r = l2norm(x, xstar)
-		bessel = kv(self.v, np.sqrt(2 * self.v) * r / self.l)
-		f = 2 ** (1 - self.v) / gamma(self.v) * (np.sqrt(2 * self.v) * r / self.l) ** self.v
-		res = f * bessel 
-		res[np.isnan(res)] = 1
-		return(res)
-	def K(self, X, Xstar):
-		r = l2norm_(X, Xstar)
-		bessel = kv(self.v, np.sqrt(2 * self.v) * r / self.l)
-		f = 2 ** (1 - self.v) / gamma(self.v) * (np.sqrt(2 * self.v) * r / self.l) ** self.v
-		res = f * bessel
-		res[np.isnan(res)] = 1
-		return(res)
+    def __init__(self, v=1, l=1):
+        self.v, self.l = v, l
+
+    def k(self, x, xstar):
+        r = l2norm(x, xstar)
+        bessel = kv(self.v, np.sqrt(2 * self.v) * r / self.l)
+        f = 2 ** (1 - self.v) / gamma(self.v) * (np.sqrt(2 * self.v) * r / self.l) ** self.v
+        res = f * bessel
+        res[np.isnan(res)] = 1
+        return (res)
+
+    def K(self, X, Xstar):
+        r = l2norm_(X, Xstar)
+        bessel = kv(self.v, np.sqrt(2 * self.v) * r / self.l)
+        f = 2 ** (1 - self.v) / gamma(self.v) * (np.sqrt(2 * self.v) * r / self.l) ** self.v
+        res = f * bessel
+        res[np.isnan(res)] = 1
+        return (res)
+
 
 class gammaExponential:
-	def __init__(self, gamma = 1, l = 1):
-		self.gamma = gamma
-		self.l = l
+    def __init__(self, gamma=1, l=1):
+        self.gamma = gamma
+        self.l = l
 
-	def K(self, X, Xstar):
-		r = l2norm_(X, Xstar)
-		return(np.exp(-(r / self.l) ** self.gamma))
+    def K(self, X, Xstar):
+        r = l2norm_(X, Xstar)
+        return (np.exp(-(r / self.l) ** self.gamma))
 
-	def gradK(self, X, Xstar, param):
-		if param == 'gamma':
-			eps = 10e-6
-			r = l2norm_(X, Xstar) + eps
-			first = -np.exp(- (r / self.l) ** self.gamma)
-			sec = (r / self.l) ** self.gamma * np.log(r / self.l)
-			gamma_grad = first * sec
-			return(gamma_grad)
-		if param == 'l':
-			r = l2norm_(X, Xstar)
-			num = self.gamma * np.exp(-(r / self.l)**self.gamma) * (r / self.l) ** self.gamma
-			l_grad = num / self.l
-			return(l_grad)
+    def gradK(self, X, Xstar, param):
+        if param == 'gamma':
+            eps = 10e-6
+            r = l2norm_(X, Xstar) + eps
+            first = -np.exp(- (r / self.l) ** self.gamma)
+            sec = (r / self.l) ** self.gamma * np.log(r / self.l)
+            gamma_grad = first * sec
+            return (gamma_grad)
+        if param == 'l':
+            r = l2norm_(X, Xstar)
+            num = self.gamma * np.exp(-(r / self.l) ** self.gamma) * (r / self.l) ** self.gamma
+            l_grad = num / self.l
+            return (l_grad)
+
 
 class rationalQuadratic:
-	def __init__(self, alpha = 1, l = 1):
-		self.alpha = alpha
-		self.l = l
+    def __init__(self, alpha=1, l=1):
+        self.alpha = alpha
+        self.l = l
 
-	def K(self, X, Xstar):
-		r = l2norm_(X, Xstar)
-		return((1 + r**2/(2 * self.alpha * self.l **2))**(-self.alpha))
+    def K(self, X, Xstar):
+        r = l2norm_(X, Xstar)
+        return ((1 + r ** 2 / (2 * self.alpha * self.l ** 2)) ** (-self.alpha))
 
-	def gradK(self, X, Xstar, param):
-		if param == 'alpha':
-			r = l2norm_(X, Xstar)
-			one = (r**2/(2 * self.alpha * self.l **2) + 1)**(-self.alpha)
-			two = r**2 / ((2 * self.alpha * self.l **2) * (r**2 / (2 * self.alpha * self.l**2) + 1))
-			three = np.log(r**2/(2* self.alpha * self.l **2) + 1)
-			alpha_grad = one * (two - three)
-			return(alpha_grad)
-		if param == 'l':
-			r = l2norm_(X, Xstar)
-			num = r ** 2 * (r ** 2 /(2 * self.alpha * self.l**2) + 1) ** (-self.alpha - 1)
-			l_grad = num / self.l ** 3
-			return(l_grad)
+    def gradK(self, X, Xstar, param):
+        if param == 'alpha':
+            r = l2norm_(X, Xstar)
+            one = (r ** 2 / (2 * self.alpha * self.l ** 2) + 1) ** (-self.alpha)
+            two = r ** 2 / ((2 * self.alpha * self.l ** 2) * (r ** 2 / (2 * self.alpha * self.l ** 2) + 1))
+            three = np.log(r ** 2 / (2 * self.alpha * self.l ** 2) + 1)
+            alpha_grad = one * (two - three)
+            return (alpha_grad)
+        if param == 'l':
+            r = l2norm_(X, Xstar)
+            num = r ** 2 * (r ** 2 / (2 * self.alpha * self.l ** 2) + 1) ** (-self.alpha - 1)
+            l_grad = num / self.l ** 3
+            return (l_grad)
+
+
 class arcSin:
-	def __init__(self, n, sigma = None):
-		if sigma == None:
-			self.sigma = np.eye(n)
-		else:
-			self.sigma = sigma
-	def k(self, x, xstar):
-		num = 2 * np.dot(np.dot(x[np.newaxis, :], self.sigma), xstar)
-		a = 1 + 2 * np.dot(np.dot(x[np.newaxis, :], self.sigma), x)
-		b = 1 + 2 * np.dot(np.dot(xstar[np.newaxis, :], self.sigma), xstar)
-		res = num / np.sqrt(a * b)
-		return(res)		
+    def __init__(self, n, sigma=None):
+        if sigma == None:
+            self.sigma = np.eye(n)
+        else:
+            self.sigma = sigma
+
+    def k(self, x, xstar):
+        num = 2 * np.dot(np.dot(x[np.newaxis, :], self.sigma), xstar)
+        a = 1 + 2 * np.dot(np.dot(x[np.newaxis, :], self.sigma), x)
+        b = 1 + 2 * np.dot(np.dot(xstar[np.newaxis, :], self.sigma), xstar)
+        res = num / np.sqrt(a * b)
+        return (res)
