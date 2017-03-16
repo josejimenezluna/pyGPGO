09b8f036ef8208bef6f5336dcf05dc8bd065a7a9
hawk31 <hawk31@gmail.com>
2017-03-16 14:00:29 +0100

    Python packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyGPGO',
+    version='0.1',
+    description='Bayesian Optimization tools in Python',
+    url='https://github.com/hawk31/pyGPGO',
+    author='Jose Jimenez',
+    author_email='jose.jimenez@upf.edu',
+    license='MIT',
+    packages=['pyGPGO'],
+    zip_safe=False)
