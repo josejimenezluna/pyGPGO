Correct noise variance computation.

examples/covzoo.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     # Build synthetic data (sine function)
-    x = np.arange(0, 2 * np.pi + 0.01, step=np.pi / 2)
+    x = np.arange(0, 2 * np.pi + 0.01, step=np.pi / 2.05)
     y = np.sin(x)
     X = np.array([np.atleast_2d(u) for u in x])[:, 0]
 
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     plt.figure()
     plt.rc('text', usetex=True)
     for i, cov in enumerate(covfuncs):
-        gp = GPRegressor(cov)
+        gp = GPRegressor(cov, optimize=True, usegrads=False)
         gp.fit(X, y)
         xstar = np.arange(0, 2 * np.pi, step=0.01)
         Xstar = np.array([np.atleast_2d(u) for u in xstar])[:, 0]

pyGPGO/GPRegressor.py
@@ -5,9 +5,8 @@ from scipy.optimize import minimize
 
 
 class GPRegressor:
-    def __init__(self, covfunc, sigma=1e-8, optimize = False, usegrads = False):
+    def __init__(self, covfunc, optimize = False, usegrads = False):
         self.covfunc = covfunc
-        self.sigma = sigma
         self.optimize = optimize
         self.usegrads = usegrads
 
@@ -22,7 +21,7 @@ class GPRegressor:
             self.optHyp(param_key=self.covfunc.parameters, param_bounds=self.covfunc.bounds, grads=grads)
 
         self.K = self.covfunc.K(self.X, self.X)
-        self.L = cholesky(self.K + self.sigma * np.eye(self.nsamples)).T
+        self.L = cholesky(self.K).T
         self.alpha = solve(self.L.T, solve(self.L, y))
         self.logp = -.5 * np.dot(self.y, self.alpha) - np.sum(np.log(np.diag(self.L))) - self.nsamples / 2 * np.log(
             2 * np.pi)
@@ -32,7 +31,7 @@ class GPRegressor:
         covfunc = self.covfunc.__class__(**k_param)
         n = self.X.shape[0]
         K = covfunc.K(self.X, self.X)
-        L = cholesky(K + self.sigma * np.eye(n)).T
+        L = cholesky(K).T
         alpha = solve(L.T, solve(L, self.y))
         inner = np.dot(np.atleast_2d(alpha).T, np.atleast_2d(alpha)) - np.linalg.inv(K)
         grads = []

pyGPGO/covfunc.py
@@ -7,16 +7,43 @@ def l2norm_(X, Xstar):
     return cdist(X, Xstar)
 
 
+def kronDelta(X, Xstar):
+    n, m = X.shape[0], Xstar.shape[0]
+    mat = np.zeros((n, m), dtype=np.int)
+    for i in range(n):
+        for j in range(m):
+            if np.array_equal(X[i], Xstar[j]):
+                mat[i, j] = 1
+    return mat
+
+
+default_bounds = {
+    'l': [1e-3, 1e4],
+    'sigmaf': [1e-3, 1e4],
+    'sigman': [1e-6, 1e4],
+    'v': [1e-3, 10],
+    'gamma': [1e-3, 1.99],
+    'alpha': [1e-3, 1e4]
+}
+
+
 class squaredExponential:
-    def __init__(self, l=1, sigmaf=1.0, bounds=[[10e-4, 10e3], [10e-4, 10e3]]):
+    def __init__(self, l=1, sigmaf=1.0, sigman=1e-6, bounds=None, parameters=['l', 'sigmaf',
+                                                                              'sigman']):
         self.l = l
         self.sigmaf = sigmaf
-        self.parameters = ['l', 'sigmaf']
-        self.bounds = bounds
+        self.sigman = sigman
+        self.parameters = parameters
+        if bounds is not None:
+            self.bounds = bounds
+        else:
+            self.bounds = []
+            for param in self.parameters:
+                self.bounds.append(default_bounds[param])
 
     def K(self, X, Xstar):
         r = l2norm_(X, Xstar)
-        return self.sigmaf * (np.exp(-.5 * r ** 2 / self.l ** 2))
+        return self.sigmaf * (np.exp(-.5 * r ** 2 / self.l ** 2)) + self.sigman * kronDelta(X, Xstar)
 
     def gradK(self, X, Xstar, param='l'):
         if param == 'l':
@@ -25,21 +52,34 @@ class squaredExponential:
             den = self.l ** 3
             l_grad = num / den
             return (l_grad)
-        if param == 'sigmaf':
+        elif param == 'sigmaf':
             r = l2norm_(X, Xstar)
             sigmaf_grad = (np.exp(-.5 * r ** 2 / self.l ** 2))
             return (sigmaf_grad)
+
+        elif param == 'sigman':
+            sigman_grad = kronDelta(X, Xstar)
+            return (sigman_grad)
+
         else:
             raise ValueError('Param not found')
 
 
 class matern:
-    def __init__(self, v=1, l=1, sigmaf=1, bounds=[[10e-4, 10e3], [10e-4, 10e3],
-                                                   [10e-4, 10e3]]):
+    def __init__(self, v=1, l=1, sigmaf=1, sigman=1e-6, bounds=None, parameters=['v',
+                                                                                 'l',
+                                                                                 'sigmaf',
+                                                                                 'sigman']):
         self.v, self.l = v, l
         self.sigmaf = sigmaf
-        self.parameters = ['v', 'l', 'sigmaf']
-        self.bounds = bounds
+        self.sigman = sigman
+        self.parameters = parameters
+        if bounds is not None:
+            self.bounds = bounds
+        else:
+            self.bounds = []
+            for param in self.parameters:
+                self.bounds.append(default_bounds[param])
 
     def K(self, X, Xstar):
         r = l2norm_(X, Xstar)
@@ -47,22 +87,31 @@ class matern:
         f = 2 ** (1 - self.v) / gamma(self.v) * (np.sqrt(2 * self.v) * r / self.l) ** self.v
         res = f * bessel
         res[np.isnan(res)] = 1
-        res = self.sigmaf * res
+        res = self.sigmaf * res + self.sigman * kronDelta(X, Xstar)
         return (res)
 
 
 class gammaExponential:
-    def __init__(self, gamma=1, l=1, sigmaf=1, bounds=[[10e-4, 1.99], [10e-4, 10e3],
-                                                       [10e-4, 10e3]]):
+    def __init__(self, gamma=1, l=1, sigmaf=1, sigman=1e-6, bounds=None, parameters=['gamma',
+                                                                                     'l',
+                                                                                     'sigmaf',
+                                                                                     'sigman']):
         self.gamma = gamma
         self.l = l
         self.sigmaf = sigmaf
-        self.parameters = ['gamma', 'l', 'sigmaf']
-        self.bounds = bounds
+        self.sigman = sigman
+        self.parameters = parameters
+        if bounds is not None:
+            self.bounds = bounds
+        else:
+            self.bounds = []
+            for param in self.parameters:
+                self.bounds.append(default_bounds[param])
 
     def K(self, X, Xstar):
         r = l2norm_(X, Xstar)
-        return self.sigmaf * (np.exp(-(r / self.l) ** self.gamma))
+        return self.sigmaf * (np.exp(-(r / self.l) ** self.gamma)) + \
+               self.sigman * kronDelta(X, Xstar)
 
     def gradK(self, X, Xstar, param):
         if param == 'gamma':
@@ -72,29 +121,43 @@ class gammaExponential:
             sec = (r / self.l) ** self.gamma * np.log(r / self.l)
             gamma_grad = first * sec
             return (gamma_grad)
-        if param == 'l':
+        elif param == 'l':
             r = l2norm_(X, Xstar)
             num = self.gamma * np.exp(-(r / self.l) ** self.gamma) * (r / self.l) ** self.gamma
             l_grad = num / self.l
             return (l_grad)
-        if param == 'sigmaf':
+        elif param == 'sigmaf':
             r = l2norm_(X, Xstar)
             sigmaf_grad = (np.exp(-(r / self.l) ** self.gamma))
             return (sigmaf_grad)
+        elif param == 'sigman':
+            sigman_grad = kronDelta(X, Xstar)
+            return (sigman_grad)
+        else:
+            raise ValueError('Param not found')
 
 
 class rationalQuadratic:
-    def __init__(self, alpha=1, l=1, sigmaf=1, bounds=[[10e-4, 10e3], [10e-4, 10e3],
-                                                       [10e-4, 10e3]]):
+    def __init__(self, alpha=1, l=1, sigmaf=1, sigman=1e-6, bounds=None, parameters = ['alpha',
+                                                                                       'l',
+                                                                                       'sigmaf',
+                                                                                       'sigman']):
         self.alpha = alpha
         self.l = l
         self.sigmaf = sigmaf
-        self.parameters = ['alpha', 'l', 'sigmaf']
-        self.bounds = bounds
+        self.sigman = sigman
+        self.parameters = parameters
+        if bounds is not None:
+            self.bounds = bounds
+        else:
+            self.bounds = []
+            for param in self.parameters:
+                self.bounds.append(default_bounds[param])
 
     def K(self, X, Xstar):
         r = l2norm_(X, Xstar)
-        return self.sigmaf * ((1 + r ** 2 / (2 * self.alpha * self.l ** 2)) ** (-self.alpha))
+        return self.sigmaf * ((1 + r ** 2 / (2 * self.alpha * self.l ** 2)) ** (-self.alpha))\
+               + self.sigman * kronDelta(X, Xstar)
 
     def gradK(self, X, Xstar, param):
         if param == 'alpha':
@@ -104,14 +167,20 @@ class rationalQuadratic:
             three = np.log(r ** 2 / (2 * self.alpha * self.l ** 2) + 1)
             alpha_grad = one * (two - three)
             return (alpha_grad)
-        if param == 'l':
+        elif param == 'l':
             r = l2norm_(X, Xstar)
             num = r ** 2 * (r ** 2 / (2 * self.alpha * self.l ** 2) + 1) ** (-self.alpha - 1)
             l_grad = num / self.l ** 3
             return (l_grad)
-        if param == 'sigmaf':
+        elif param == 'sigmaf':
             r = l2norm_(X, Xstar)
-            return ((1 + r ** 2 / (2 * self.alpha * self.l ** 2)) ** (-self.alpha))
+            sigmaf_grad = (1 + r ** 2 / (2 * self.alpha * self.l ** 2)) ** (-self.alpha)
+            return (sigmaf_grad)
+        elif param == 'sigman':
+            sigman_grad = kronDelta(X, Xstar)
+            return (sigman_grad)
+        else:
+            raise ValueError('Param not found')
 
 
 class arcSin:

testing/testing.py
@@ -1,18 +1,20 @@
-import numpy as np
 from testing.utils import *
 from testing.modaux import *
 
 if __name__ == '__main__':
 
-    models = [RF()]
-    params = [d_rf]
+    models = [GBM()]
+    params = [d_gbm]
 
 
     path = os.path.join(os.getcwd(), 'datasets')
-    datasets = ['aff.csv','breast_cancer.csv', 'indian_liver.csv', 'parkinsons.csv',
-                'lsvt.csv', 'pima-indians-diabetes.csv']
-    problems = ['cont', 'binary', 'binary', 'binary', 'binary', 'binary']
-    targets = [0, 0, 10, 16, 0, 8]
+    #datasets = ['aff.csv','breast_cancer.csv', 'indian_liver.csv', 'parkinsons.csv',
+    #            'lsvt.csv', 'pima-indians-diabetes.csv']
+    #problems = ['cont', 'binary', 'binary', 'binary', 'binary', 'binary']
+    #targets = [0, 0, 10, 16, 0, 8]
+    datasets = ['pocket_little.csv']
+    problems = ['binary']
+    targets = [0]
 
     np.random.seed(20)
     for model, parameter_dict in zip(models, params):