dbd7a5a005db80a5af2059d1ac2596a11d695a70
hawk31 <jose.jimenez@upf.edu>
2017-03-25 11:42:34 +0100

    conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,7 @@ extensions = [
 
 napoleon_include_special_with_doc = True
 napoleon_include_private_with_doc = True
+napoleon_include_init_with_doc = True
 
 
 # Add any paths that contain templates here, relative to this directory.
